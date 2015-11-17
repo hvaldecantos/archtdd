853ffa25df1434a27f98bb8ccffd89c1408b7f1e
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-17 18:39:36 -0500

    Use variables for name and password in test

--- user_spec.rb
+++ user_spec.rb
@@ -7,12 +7,14 @@ include BCrypt
 describe User do
 
   before do
-    @user = User.new "Fred", "secret"
+    @password = "secret"
+    @name = "Fred"
+    @user = User.new @name, @password
   end
   it "has a name" do
-    @user.name.must_equal "Fred"
+    @user.name.must_equal @name
   end
   it "has an encrypted password" do
-    @user.password.must_equal BCrypt::Engine.hash_secret("secret", @user.password)
+    @user.password.must_equal BCrypt::Engine.hash_secret(@password, @user.password)
   end
 end
