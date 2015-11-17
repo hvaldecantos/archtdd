35138fe205035c06096cd4444800c5d150139c0b
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-17 18:22:52 -0500

    [breaker] A user has an encrypted password

--- user_spec.rb
+++ user_spec.rb
@@ -1,14 +1,18 @@
 require 'minitest/autorun'
 require './user'
+require 'bcrypt'
+
+include BCrypt
 
 describe User do
+
   before do
     @user = User.new "Fred", "secret"
   end
   it "has a name" do
     @user.name.must_equal "Fred"
   end
-  it "has a password" do
-    @user.password.must_equal "secret"
+  it "has an encrypted password" do
+    @user.password.must_equal BCrypt::Engine.hash_secret("secret", @user.password)
   end
 end
