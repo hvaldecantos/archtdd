8d87adb15aeb4dfdcc0aac3f06f391b84f76a6e6
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-17 23:03:57 -0500

    add db schema table users

---
+++ db_schema.rb
@@ -0,0 +1,14 @@
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :name
+    t.string :password
+  end
+end
