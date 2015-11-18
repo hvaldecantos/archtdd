08e5d03070fc75d43ed6734a69dea9b7bf8db2c8
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-18 13:51:33 -0500

    Authenticate method returns nil when using invalid credentials

--- xxxx.rb
+++ xxxx.rb
@@ -2,13 +2,17 @@ require 'minitest/autorun'
 require './user'
 
 def authenticate user, pass
-  ""
+  User.find_by_name(user)
 end
 
 describe "Authentication" do
   it "should authenticate a user" do
-    user = User.new name: "Carlos", password: "1234", password_confirmation: "1234"
+    User.new(name: "Carlos", password: "1234", password_confirmation: "1234").save
     token = authenticate("Carlos", "1234")
     token.wont_be_nil
+  end
+  it "should not authenticate with an invalid credentials" do
+    token = authenticate("", "pass")
+    token.must_be_nil
   end
 end
