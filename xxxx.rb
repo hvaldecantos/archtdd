becf82e392330ed1f247287581077ded8101b5fc
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-18 14:08:34 -0500

    Authenticate returns nil with wrong password

--- xxxx.rb
+++ xxxx.rb
@@ -2,7 +2,9 @@ require 'minitest/autorun'
 require './user'
 
 def authenticate user, pass
-  User.find_by_name(user)
+  user = User.find_by_name(user)
+  return user if user == nil
+  user.password == BCrypt::Engine.hash_secret(pass, user.password) ? user : nil
 end
 
 describe "Authentication" do
@@ -11,8 +13,13 @@ describe "Authentication" do
     token = authenticate("Carlos", "1234")
     token.wont_be_nil
   end
-  it "should not authenticate with an invalid credentials" do
-    token = authenticate("", "pass")
+  it "should not authenticate with an invalid user" do
+    token = authenticate("invalid", "pass")
+    token.must_be_nil
+  end
+  it "should not authenticate with a wrong password" do
+    User.new(name: "Carlos", password: "1234", password_confirmation: "1234").save
+    token = authenticate("Carlos", "pass")
     token.must_be_nil
   end
 end
