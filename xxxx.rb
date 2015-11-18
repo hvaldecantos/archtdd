3353e02f7c4b2e93c51410b2bbe8120b56432d44
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-18 14:21:32 -0500

    Authenticate method returns true or false

--- xxxx.rb
+++ xxxx.rb
@@ -3,23 +3,20 @@ require './user'
 
 def authenticate user, pass
   user = User.find_by_name(user)
-  return user if user == nil
-  user.password == BCrypt::Engine.hash_secret(pass, user.password) ? user : nil
+  return false if user == nil
+  user.password == BCrypt::Engine.hash_secret(pass, user.password)
 end
 
 describe "Authentication" do
   it "should authenticate a user" do
     User.new(name: "Carlos", password: "1234", password_confirmation: "1234").save
-    token = authenticate("Carlos", "1234")
-    token.wont_be_nil
+    authenticate("Carlos", "1234").must_equal true
   end
   it "should not authenticate with an invalid user" do
-    token = authenticate("invalid", "pass")
-    token.must_be_nil
+    authenticate("invalid", "pass").must_equal false
   end
   it "should not authenticate with a wrong password" do
     User.new(name: "Carlos", password: "1234", password_confirmation: "1234").save
-    token = authenticate("Carlos", "pass")
-    token.must_be_nil
+    authenticate("Carlos", "pass").must_equal false
   end
 end
