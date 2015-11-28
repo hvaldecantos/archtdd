89c66d2d2e12121743b350e071d6de7b230c8af9
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-28 15:34:33 -0500

    name xxxx to session_manager_spec

---
+++ session_manager.rb
@@ -0,0 +1,11 @@
+require './user'
+require './session'
+
+class SessionManager
+
+  def self.authenticate user, pass
+    user = User.find_by_name(user)
+    return false if user == nil
+    user.password == BCrypt::Engine.hash_secret(pass, user.password)
+  end
+end
---
+++ session_manager_spec.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require './user'
+require './session'
+require './session_manager'
+
+describe "SessionManager" do
+
+  describe "Authentication" do
+    before do
+      @name = "Carlos"
+      @password = "1234"
+      User.new(name: @name, password: @password).save
+    end
+    it "should authenticate a user" do
+      SessionManager::authenticate(@name, @password).must_equal true
+    end
+    it "should not authenticate with an invalid user" do
+      SessionManager::authenticate("invalid", "pass").must_equal false
+    end
+    it "should not authenticate with a wrong password" do
+      SessionManager::authenticate(@name, "wrong").must_equal false
+    end
+  end
+
+end
