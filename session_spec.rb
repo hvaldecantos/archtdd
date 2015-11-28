5fddbd8956437e02af6a0d4ace0b3ca4e71b58b5
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-28 15:28:04 -0500

    refactor tests

---
+++ session_spec.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require './user'
+require './session'
+
+describe "Session" do
+  before do
+    @name = "Carlos"
+    @password = "1234"
+    @user = User.new(name: @name, password: @password)
+    @user.save
+  end
+  it "should have a token" do
+    session = Session.new(user: @user)
+    session.token.wont_be_nil
+  end
+  it "should have a token with size 40" do
+    session = Session.new(user: @user)
+    session.save
+    session.token.size.must_equal 40
+  end
+  describe "is_expired" do
+    it "should return false if session has not expired" do
+      session = Session.new(user: @user)
+      session.save
+      session.is_expired.must_equal false
+    end
+    it "should return true if session has expired" do
+      session = Session.new(user: @user, created_at: Time.now - Session::EXPIRE_TIMEOUT_SECONDS)
+      session.is_expired.must_equal true
+    end
+  end
+end
--- xxxx.rb
+++ xxxx.rb
@@ -1,7 +1,6 @@
 require 'minitest/autorun'
 require './user'
 require './session'
-require 'digest/sha1'
 
 def authenticate user, pass
   user = User.find_by_name(user)
@@ -23,34 +22,5 @@ describe "Authentication" do
   end
   it "should not authenticate with a wrong password" do
     authenticate(@name, "wrong").must_equal false
-  end
-end
-
-describe "Session" do
-  before do
-    @name = "Carlos"
-    @password = "1234"
-    @user = User.new(name: @name, password: @password)
-    @user.save
-  end
-  it "should have a token" do
-    session = Session.new(user: @user)
-    session.token.wont_be_nil
-  end
-  it "should have a token with size 40" do
-    session = Session.new(user: @user)
-    session.save
-    session.token.size.must_equal 40
-  end
-  describe "is_expired" do
-    it "should return false if session has not expired" do
-      session = Session.new(user: @user)
-      session.save
-      session.is_expired.must_equal false
-    end
-    it "should return true if session has expired" do
-      session = Session.new(user: @user, created_at: Time.now - Session::EXPIRE_TIMEOUT_SECONDS)
-      session.is_expired.must_equal true
-    end
   end
 end
