30a026893a287da40e425e01d017983f0cc1eacb
Carlos Dubus <carlos.garciadm@gmail.com>
2015-11-28 19:38:39 -0500

    fix identation

--- session.rb
+++ session.rb
@@ -2,14 +2,14 @@ require 'active_record'
 require './db_schema'
 
 class Session < ActiveRecord::Base
-	belongs_to :user
-	EXPIRE_TIMEOUT_SECONDS = 60
+  belongs_to :user
+  EXPIRE_TIMEOUT_SECONDS = 60
 
   def initialize opt = {}
     super opt
     self.token = "1"
   end
-	def is_expired
-		return Time.now - self.created_at > EXPIRE_TIMEOUT_SECONDS
-	end
+  def is_expired
+    return Time.now - self.created_at > EXPIRE_TIMEOUT_SECONDS
+  end
 end
