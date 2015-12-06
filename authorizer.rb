333c034ee6f099ea86bac3e9adb0407d57fb525c
Carlos Dubus <carlos.garciadm@gmail.com>
2015-12-05 23:19:29 -0500

    authorizer should add action

--- authorizer.rb
+++ authorizer.rb
@@ -1,5 +1,12 @@
 class Authorizer
+  @@actions = []
+  def self.actions
+    @@actions
+  end
   def self.is_authorized? user, required_role
     user.has_role?(required_role)
+  end
+  def self.add_action action
+  	@@actions << action
   end
 end
--- authorizer_spec.rb
+++ authorizer_spec.rb
@@ -13,4 +13,9 @@ describe "Authorization" do
     required_role = "admin"
     Authorizer::is_authorized?(user, required_role).must_equal false
   end
+  it "should add action" do
+    action = {action: "list_users", role: "admin"}
+    Authorizer.add_action(action)
+    Authorizer.actions.include?(action).must_equal true
+  end
 end
