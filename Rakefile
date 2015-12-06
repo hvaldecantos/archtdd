6f822d103f539223adb31895dcefd29963c967eb
Carlos Dubus <carlos.garciadm@gmail.com>
2015-12-06 00:12:56 -0500

    use memory database if env equals test

--- Rakefile
+++ Rakefile
@@ -2,5 +2,6 @@ require 'rake'
 require 'rake/testtask'
 
 Rake::TestTask.new do |t|
+  ENV["ENV"] = "test"
   t.pattern = '*_spec.rb'
 end
--- db_schema.rb
+++ db_schema.rb
@@ -1,21 +1,30 @@
 require 'sqlite3'
 require 'active_record'
 
+if ENV["ENV"] == "test"
+  db = ":memory:"
+else
+  db = "database.db"
+end
+
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: ':memory:'
+  database: db
 )
 
 ActiveRecord::Schema.define do
-  create_table :users, force: true do |t|
-    t.string :name
-    t.string :password
-    t.string :role, default: "guest"
+  if !ActiveRecord::Base.connection.table_exists?(:users)
+    create_table :users, force: true do |t|
+      t.string :name
+      t.string :password
+      t.string :role, default: "guest"
+    end
   end
-  create_table :sessions, force: true do |t|
-    # t.string :token
-    t.belongs_to :user, index: true
-    t.datetime :created_at
-    t.string :token
+  if !ActiveRecord::Base.connection.table_exists?(:sessions)
+    create_table :sessions, force: true do |t|
+      t.belongs_to :user, index: true
+      t.datetime :created_at
+      t.string :token
+    end
   end
 end
