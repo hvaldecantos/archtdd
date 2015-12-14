665bdfe6f65082d9054b49d7c9d244786fa7c95f
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-14 16:40:18 -0500

    add new user to cli

--- cli_runner.rb
+++ cli_runner.rb
@@ -9,12 +9,12 @@ class CLIRunner
   end  
 
   def self.instanciate_cmd_object cmd
-    name = "Carlos"
-    password = "1234"
-    User.create(name: name, password: password)
     if cmd.keys.include? :login
       args = eval(cmd[:login])
       puts SessionManager::authenticate(args[:name], args[:pass])
+    elsif cmd.keys.include? :new_user
+      args = eval(cmd[:new_user])
+      User.create(name: args[:name], password: args[:pass])
     else
       p "no args"
     end
@@ -24,9 +24,11 @@ class CLIRunner
     cmd = []
     values = {}
     OptionParser.new do |opts|
-      opts.banner = "\n\t--login=\"['some@email.com','password']\"\n"
-      opts.separator "Command line..."
-      opts.on('--login array', 'Login into the system'){|o| cmd << '--login'; values[:login] = o }
+      opts.banner = "\n\t--login \"{name: 'username', pass: '1234'}\"" +
+                    "\n\t--new-user \"{name: 'username', pass: '1234'}\"\n"
+      opts.separator "Command line ."
+      opts.on('--login <hash>', 'Login into the system'){|o| cmd << '--login'; values[:login] = o }
+      opts.on('--new-user <hash>', 'Add a new user in the system'){|o| cmd << '--new-user'; values[:new_user] = o }
     end.parse!(argv)
     values
   end
