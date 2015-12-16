2b8e43c5a01f414d302aa68fa89de839d514644f
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-16 11:17:07 -0500

    add --list-users command line

--- Readme.md
+++ Readme.md
@@ -4,6 +4,16 @@ Uncovering architecture breakers through the process of Test Driven Development
 
 ## Run the cli
 
+### Help
+
+```
+ruby cli_runner.rb --help
+```
+
+### Commands
+
 ```
 ruby cli_runner.rb --login "{name: 'name', pass: '1234'}"
+ruby cli_runner.rb --new-user "{name: 'Fred', password: '1234', role: 'admin'}"
+ruby cli_runner.rb --list-users "{name: 'Fred', token: 'token'}"
 ```
--- cli_runner.rb
+++ cli_runner.rb
@@ -1,36 +1,56 @@
 require 'optparse'
 require './session_manager'
+require './authorizer'
 require './user'
 
 class CLIRunner
   def self.run argv
-    cmd_opts = parse argv
-    instanciate_cmd_object cmd_opts
+    cmd_opts, symb = parse(argv)
+    instanciate_cmd_object cmd_opts, symb
   end  
 
-  def self.instanciate_cmd_object cmd
+  def self.instanciate_cmd_object cmd, symb
+    args = eval(cmd[symb])
+
     if cmd.keys.include? :login
-      args = eval(cmd[:login])
-      puts SessionManager::authenticate(args[:name], args[:pass])
+      puts "logging in     ----------------------"
+      p SessionManager::login(args[:name], args[:password])
     elsif cmd.keys.include? :new_user
-      args = eval(cmd[:new_user])
-      User.create(name: args[:name], password: args[:pass])
+      puts "creating users ----------------------"
+      p User.create(args).save!
+    elsif cmd.keys.include? :list_users
+      puts "listing users  ----------------------"
+      Authorizer.add_action({action: :list_users, role: "admin"})
+      if SessionManager::is_valid_token?(args[:token])
+        puts "> authenticated through token"
+        if Authorizer::is_authorized?(User.find_by_name(args[:name]), :list_users)
+          puts "> authorized"
+          User.all.each{|u| puts u.name}
+        else
+          puts "> Unauthorized"
+        end
+      else
+        puts "> Invalid token"
+      end
     else
-      p "no args"
+      p "> no args"
     end
   end
 
   def self.parse argv
-    cmd = []
+    cmd = ""
     values = {}
     OptionParser.new do |opts|
-      opts.banner = "\n\t--login \"{name: 'username', pass: '1234'}\"" +
-                    "\n\t--new-user \"{name: 'username', pass: '1234'}\"\n"
-      opts.separator "Command line ."
-      opts.on('--login <hash>', 'Login into the system'){|o| cmd << '--login'; values[:login] = o }
-      opts.on('--new-user <hash>', 'Add a new user in the system'){|o| cmd << '--new-user'; values[:new_user] = o }
+      opts.banner = "\nOptions for <hash> according command:" +
+                    "\n\t--login \"{name: 'username', password: '1234'}\"" +
+                    "\n\t--list-users \"{name: 'username', token: 'token'}\"" +
+                    "\n\t--new-user \"{name: 'username', password: '1234', role: 'admin'}\"\n"
+      opts.separator "Command line:"
+      opts.on('--login <hash>', 'Login into the system'){|o| cmd = :login; values[:login] = o }
+      opts.on('--new-user <hash>', 'Add a new user in the system'){|o| cmd = :new_user; values[:new_user] = o }
+      opts.on('--list-users <hash>', 'Add a new user in the system'){|o| cmd = :list_users; values[:list_users] = o }
     end.parse!(argv)
-    values
+    [values, cmd]
   end
 end
 
